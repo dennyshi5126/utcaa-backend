8e6f0cfdfca162366fa7044f4555e6b1b017401c
Qisen Wang <qisenwang@QisendeMacBook-Air.local>
2019-09-30 22:16:20 -0400

    prototype01

--- app/controllers/publicRoutes/index.js
+++ app/controllers/publicRoutes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import healthControllers from './healthControllers';
+import userControllers from './userControllers';
 const publicRouter = express.Router();
 
 publicRouter.route('/health').get(healthControllers.check);
-
+publicRouter.route('/users/password').post(userControllers.confirmForgetPassword);
 export default publicRouter;
