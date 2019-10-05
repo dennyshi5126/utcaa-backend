c374fe77498ee90d7b88444d86f68be7d58fad05
yuqingzou <yisubai@icloud.com>
2019-10-04 20:31:39 -0400

    removing the extra file forgetpasswordcontorller and moved router in publicRouter/index

--- app/controllers/publicRoutes/index.js
+++ app/controllers/publicRoutes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import healthControllers from './healthControllers';
+import userControllers from './userControllers';
+
 const publicRouter = express.Router();
 
 publicRouter.route('/health').get(healthControllers.check);
+publicRouter.route('/public/users/password/reset').post(userControllers.forgetPassword);
 
 export default publicRouter;
--- app/controllers/userForgetPasswordRoute/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import publicUserController from '../publicRoutes/userControllers';
-
-const forgetPasswordRouter = express.Router();
-
-forgetPasswordRouter.route('/public/users/password/reset').post(publicUserController.forgetPassword);
-
-export default forgetPasswordRouter;
