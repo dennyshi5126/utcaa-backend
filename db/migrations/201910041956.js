f1cc31980e135e3f1b7522c748f01454cda7ded6
Trinity <ohmangogo@gmail.com>
2019-10-05 00:48:31 -0400

    added 'down' and department data

--- db/migrations/201910041956.js
+++ db/migrations/201910041956.js
@@ -2,20 +2,38 @@
 
 const up = function(queryInterface, Sequelize) {
   console.log('creating departments...');
-  return queryInterface.createTable('departments', {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-      isUnique: true,
-    },
-    name: {
-      type: Sequelize.STRING(128),
-      allowNull: false,
-      isUnique: true,
-    },
-  });
+  return queryInterface
+    .createTable('departments', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        isUnique: true,
+      },
+      name: {
+        type: Sequelize.STRING(128),
+        allowNull: false,
+        isUnique: true,
+      },
+    })
+    .then(function() {
+      console.log('inserting departments data...');
+      return queryInterface.bulkInsert(
+        'departments'[
+          ({ name: '活动部 Events' },
+          { name: '赞助部 Sponsorship' },
+          { name: '市场部 Marketing' },
+          { name: '人事部 HR' })
+        ],
+        {}
+      );
+    });
 };
 
-module.exports = { up };
+const down = function(queryInterface) {
+  console.log('dropping departments...');
+  return queryInterface.dropTable('departments');
+};
+
+module.exports = { up, down };
