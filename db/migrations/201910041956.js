7c028c5ebcceb65ecf452f384ee6f35ab72ef6e7
Trinity <ohmangogo@gmail.com>
2019-10-07 23:26:09 -0400

    fixed insert data

--- db/migrations/201910041956.js
+++ db/migrations/201910041956.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const up = function(queryInterface, Sequelize) {
-  console.log('creating departments...');
+  console.log('creating departments table...');
   return queryInterface
     .createTable('departments', {
       id: {
@@ -19,20 +19,17 @@ const up = function(queryInterface, Sequelize) {
     })
     .then(function() {
       console.log('inserting departments data...');
-      return queryInterface.bulkInsert(
-        'departments'[
-          ({ name: '活动部 Events' },
-          { name: '赞助部 Sponsorship' },
-          { name: '市场部 Marketing' },
-          { name: '人事部 HR' })
-        ],
-        {}
-      );
+      return queryInterface.bulkInsert('departments', [
+        { name: '活动部 Events' },
+        { name: '赞助部 Sponsorship' },
+        { name: '市场部 Marketing' },
+        { name: '人事部 HR' },
+      ]);
     });
 };
 
 const down = function(queryInterface) {
-  console.log('dropping departments...');
+  console.log('dropping departments table...');
   return queryInterface.dropTable('departments');
 };
 
