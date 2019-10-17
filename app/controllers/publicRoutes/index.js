26a2c2f5596afe426736121dc9af15b4d2b5811e
yuqingzou <yisubai@icloud.com>
2019-10-16 23:54:20 -0400

    rebaseing to master

--- app/controllers/publicRoutes/index.js
+++ app/controllers/publicRoutes/index.js
@@ -10,5 +10,6 @@ publicRouter.route('/users/password').post(userControllers.confirmForgetPassword
 publicRouter.route('/departments').get(departmentControllers.list);
 publicRouter.route('/register').post(userControllers.signup);
 publicRouter.route('/user/signin').post(userControllers.signin);
+publicRouter.route('/users/password/reset').post(userControllers.forgetPassword);
 
 export default publicRouter;
