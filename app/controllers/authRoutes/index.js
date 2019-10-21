5937bbb1caad1bbe219aad75e69bf37cc29e69bf
dennyshi5126 <dennyshi16@gmail.com>
2019-10-22 05:50:47 +0800

    Clean up all commented codes

--- app/controllers/authRoutes/index.js
+++ app/controllers/authRoutes/index.js
@@ -3,7 +3,6 @@ import userControllers from './userControllers';
 
 const authRouter = express.Router();
 
-//authRouter.route('/users/:userId/avatar').post(userControllers.uploadAvatar);
 authRouter.route('/users/authenticate').get(userControllers.authenticate);
 authRouter.route('/users/:userId/signout').post(userControllers.signout);
 authRouter.route('/users/:userId').patch(userControllers.updateSelfProfile);
--- app/controllers/middlewares/authentication.js
+++ app/controllers/middlewares/authentication.js
@@ -2,15 +2,12 @@ import { ERROR_TYPES as errors } from '../../utils/errors';
 import { isAuthenticated } from '../../security/authProvider';
 import { getUserRequestHeaders } from '../../utils/cookieHelpers';
 import users from '../../entities/user';
-//import groupRepository from '../../repositories/groups';
 
 export default async function(req, _res, next) {
   const { userId, userEmail, userSessionId } = getUserRequestHeaders(req.headers);
   try {
     await isAuthenticated(userId, userEmail, userSessionId);
     const user = await users.getById(userId);
-    //const group = await groupRepository.getById(user.groupId);
-    //user.group = group;
     req.user = user;
     next();
   } catch (error) {
